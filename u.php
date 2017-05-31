3e37412f30446a133869688bf7e156180a70c10b
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-05-31 10:31:35 -0600

    add redirect

---
+++ u.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Redirect (URL shortening) for a community chat board.
+ */
+//Write response code
+http_response_code(301);
+//Write redirect header
+header("Location: https://github.com/jspenguin2017/uBlockProtector");
