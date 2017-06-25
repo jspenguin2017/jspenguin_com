8c1a66801545aae2b4316b6b6751888e1f85adca
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2017-06-24 18:47:06 -0600

    Update API.php

--- API/uBlockProtector/API.php
+++ API/uBlockProtector/API.php
@@ -9,7 +9,7 @@ require("Config.php");
 if ($key === filter_input(INPUT_POST, "key", FILTER_UNSAFE_RAW)) {
     preg_match("/^\d+\.\d+$/", filter_input(INPUT_POST, "data", FILTER_UNSAFE_RAW), $match);
     if (count($match) === 1) {
-        if (file_put_contents('Data.txt', $match[0])) {
+        if (file_put_contents("Data.txt", $match[0])) {
             echo "ok";
         } else {
             echo "could not save";
